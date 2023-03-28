7090554710b758d82292e939f76ad91ca24f8272
Alejandro Iván Lizárraga Rojas <alejandroivanliza@gmail.com>
2023-03-28 00:53:30 -0700

    Proyecto Practica 2 Alumno

---
+++ app.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const express = require("express");
+
+const bodyParser = require("body-parser");
+const { urlencoded } = require("body-parser");
+
+const rutas = require("./router/index");
+const path = require("path");
+
+const app = express();
+app.use(express.json())
+app.use(express.static(__dirname + "/public/"));
+app.set("view engine", "ejs");
+app.use(bodyParser(urlencoded({ extended: true })));
+
+// Cambiar extensiones ejs a html
+app.engine("html", require("ejs").renderFile);
+app.use(rutas);
+
+const puerto = 2500;
+app.listen(puerto, () => {
+	console.log("Iniciando puerto " + puerto);
+});
